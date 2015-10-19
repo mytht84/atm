00e50fc6960817e41672d8abcac23a3e270b2206
My <lillemymtt@hotmail.com>
2015-10-19 23:20:21 +0200

    html + jQuery test

--- atm.js
+++ atm.js
@@ -5,3 +5,21 @@
  */
 
 
+$(document).ready(function(){
+    $('#englishButton').click(function(e){
+        e.preventDefault();
+        $(this).progressTimed(2);
+    });
+    
+    $('#spainButton').click(function(e){
+        e.preventDefault();
+        $(this).progressTimed(2);
+    });
+    
+    $('#germanButton').click(function(e){
+        e.preventDefault();
+        $(this).progressTimed(2);
+    });
+  
+    
+})(jQuery);
